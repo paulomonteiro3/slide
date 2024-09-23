8d5f03934d6af40179bcaed12cad91cf057c7be0
paulomonteiro3 <paulomonteirop22@gmail.com>
2024-09-23 18:18:22 -0300

    calculo de distancia quando os eventos ocorrem

--- js/slide.js
+++ js/slide.js
@@ -1,30 +1,43 @@
-export default class Slide {
+export default class Slide{
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide)
     this.wrapper = document.querySelector(wrapper)
+    this.dist = { finalPosition: 0, startX: 0, movement: 0 }
   }
 
-  OnStart(event) {
+  moveSlde(distX) {
+    this.dist.movePosition = distX
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+  }
+
+  updatePosition(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.6
+    return this.dist.finalPosition - this.dist.movement
+  }
+  onStart(event) {
     event.preventDefault()
-    console.log('mousedown')
+    this.dist.startX = event.clientX
     this.wrapper.addEventListener('mousemove', this.onMove)
   }
-
   onMove(event) {
+    const finalPosition = this.updatePosition(event.clientX)
+    this.moveSlde(finalPosition)
   }
-  
+
   onEnd(event) {
-    this.wrapper.removeEventListener('mousemove', this.onMove)
+    this.wrapper.removeEventListener('mousemove', this.onMove);
+    this.dist.finalPosition = this.dist.movePosition
   }
 
   addSlideEvents() {
-    this.wrapper.addEventListener('mousedown', this.OnStart)
-    this.wrapper.addEventListener('mousedown', this.onEnd)
+    this.wrapper.addEventListener('mousedown', this.onStart)
+    this.wrapper.addEventListener('mouseup', this.onEnd)
   }
 
   bindEvents() {
-    this.OnStart = this.OnStart.bind(this)
+    this.onStart = this.onStart.bind(this)
     this.onMove = this.onMove.bind(this)
+    this.onEnd = this.onEnd.bind(this)
   }
 
   init() {
