cf92397f305c353af4deb1be731ac144bfd8a0d5
paulomonteiro3 <paulomonteirop22@gmail.com>
2024-09-24 11:29:50 -0300

    touchevents adicionado

--- js/slide.js
+++ js/slide.js
@@ -15,11 +15,19 @@ export default class Slide{
     return this.dist.finalPosition - this.dist.movement
   }
   onStart(event) {
-    event.preventDefault()
-    this.dist.startX = event.clientX
-    this.wrapper.addEventListener('mousemove', this.onMove)
+    let movetype
+    if (event.stype === 'mousedown') {
+      event.preventDefault()
+      this.dist.startX = event.clientX
+      movetype = 'mousemove'
+    } else {
+      this.dist.startX = event.changedTouches[0].clientX
+      movetype = 'touchmove'
+    }
+    this.wrapper.addEventListener(movetype, this.onMove)
   }
   onMove(event) {
+    const pointerPosition = (event.type === 'mousemove') ? event.clientX : event.changedTouches[0].clientX
     const finalPosition = this.updatePosition(event.clientX)
     this.moveSlde(finalPosition)
   }
@@ -31,7 +39,9 @@ export default class Slide{
 
   addSlideEvents() {
     this.wrapper.addEventListener('mousedown', this.onStart)
+    this.wrapper.addEventListener('touchstart', this.onStart)
     this.wrapper.addEventListener('mouseup', this.onEnd)
+    this.wrapper.addEventListener('touchend', this.onEnd)
   }
 
   bindEvents() {
